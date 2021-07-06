747f3d4b4cbf2e154bfbb6fffca69788b8575d1e
yun6160 <devyun6160@gmail.com>
2021-07-06 10:30:10 +0900

    변경

--- src/main/java/edu/bit/ex/HomeController_jh.java
+++ src/main/java/edu/bit/ex/HomeController_jh.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
-<<<<<<< HEAD
  * Handles requests for the application home page.
  */
 @Controller
@@ -20,22 +19,12 @@ public class HomeController_jh {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController_jh.class);
 
-=======
- 로그인 기능이 완성
- */
-@Controller
-public class HomeController_jh {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController_jh.class);
-	
->>>>>>> upstream/master
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-<<<<<<< HEAD
 
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -47,17 +36,4 @@ public class HomeController_jh {
 		return "home";
 	}
 
-=======
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
->>>>>>> upstream/master
 }
